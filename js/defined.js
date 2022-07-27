61c8d0e666f0e369dfa25e18ec1f80492db867c2
antnguyen090 <107045368+antnguyen090@users.noreply.github.com>
2022-07-28 00:18:13 +0700

    api gold, coin

---
+++ js/defined.js
@@ -0,0 +1,5 @@
+const API_PREFIX = 'http://apiforlearning.zendvn.com/api/';
+let elmAreaCategoryNews = $("ul#api-area-category-news");
+let elmAreaNewsItem = $("div#news-items");
+let elmAreaGoldPrice = $("#tableGoldPriceImport");
+let elmAreaCoinPrice = $("#tableCoinPriceImport");
